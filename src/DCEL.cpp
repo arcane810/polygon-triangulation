fc0541fe68d3a6cd40d2c3d0d110dff9393a431c
Akul Singhal <singhal.akul9@gmail.com>
2020-11-19 16:57:47 +0530

    implement handle start,end,split vertices

--- src/DCEL.cpp
+++ src/DCEL.cpp
@@ -2,11 +2,14 @@
 
 #include "DCEL.hpp"
 
-Face::Face(HalfEdge *edge) : edge(edge) {}
+Face::Face(HalfEdge *half_edge) : half_edge(half_edge) {}
 
 Vertex::Vertex(Point p) : point(p) {}
 
 HalfEdge::HalfEdge(Vertex *origin) : origin(origin) {}
+
+Edge::Edge(Vertex *v1, Vertex *v2, HalfEdge *half_edge)
+    : v1(v1), v2(v2), half_edge(half_edge) {}
 
 DCEL::DCEL(std::vector<Point> points) {
     Face outer = Face();
@@ -27,8 +30,9 @@ DCEL::DCEL(std::vector<Point> points) {
 
         // Outer HalfEdge
         HalfEdge curr_out = HalfEdge(&vertices[(i + 1) % n]);
+        half_edges.push_back(curr_out);
 
-        vertices[(i + 1) % n].edge = &curr_out;
+        vertices[(i + 1) % n].half_edge = &curr_out;
 
         curr_out.next = prev_out;
         if (prev_out != nullptr) {
@@ -39,6 +43,7 @@ DCEL::DCEL(std::vector<Point> points) {
         prev_out = &curr_out;
 
         HalfEdge curr_in = HalfEdge(&vertices[i]);
+        half_edges.push_back(curr_in);
         curr_in.twin = &curr_out;
         curr_out.twin = &curr_in;
 
@@ -49,6 +54,11 @@ DCEL::DCEL(std::vector<Point> points) {
             first_in = &curr_in;
         }
         prev_in = &curr_in;
+
+        Edge edge = Edge(&vertices[i], &vertices[(i + 1) % n], &curr_out);
+        curr_in.edge = &edge;
+        curr_out.edge = &edge;
+        edges.push_back(&edge);
     }
     prev_out->prev = first_out;
     first_out->next = prev_out;
@@ -56,7 +66,7 @@ DCEL::DCEL(std::vector<Point> points) {
     prev_in->next = first_in;
     first_in->prev - prev_in;
 
-    inner.edge = first_in;
+    inner.half_edge = first_in;
 
-    outer.edge = first_out;
+    outer.half_edge = first_out;
 }
